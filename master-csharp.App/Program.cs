4d969612f0af489e45104d8dc4660cd43b3e3734
MohamedMagdy945 <mohamedmagdy9452@gmail.com>
2026-02-24 16:44:29 +0200

    init

---
+++ master-csharp.App/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace master_csharp.App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
