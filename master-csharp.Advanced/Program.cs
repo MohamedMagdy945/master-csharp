a2295c9b8cc8044e46dbf51bea1397a3fc071b6c
MohamedMagdy945 <mohamedmagdy9452@gmail.com>
2026-02-27 20:13:29 +0200

    edit:File path

---
+++ master-csharp.Advanced/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace master_csharp.Advanced
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            StructUsage.Test();
+
+        }
+}
+}
