60760aacd3cd631281f2e2d49d2fa387453054fd
MohamedMagdy945 <mohamedmagdy9452@gmail.com>
2026-03-01 17:05:11 +0200

    feat:casting operator overlaod

---
+++ master-csharp.Advanced/OperatorOverLoad/Meter.cs
@@ -0,0 +1,47 @@
+﻿
+
+
+using master_csharp.Advanced.OperatorOverLoad;
+
+namespace master_csharp.Advanced.OperatorOverLoad
+{
+    class CM
+    {
+        public double Value;
+
+    }
+    class Meter
+    {
+        public double Value;
+
+        public Meter(double value)
+        {
+            Value = value;
+        }
+
+        public static implicit operator Meter(double value)
+        {
+            return new Meter(value);
+        }
+        public static implicit operator double(Meter value)
+        {
+            return value.Value;
+        }
+        public static explicit operator CM(Meter meter)
+        {
+            return new CM() { Value = meter.Value*100};
+        }
+    }
+}
+class CastingOperatorOverload()
+{
+    public static void Use()
+    {
+        Meter m = new Meter(4);
+
+        m = 8;
+
+        CM cm = (CM)m;
+        Console.WriteLine(cm.Value); // 800
+    }
+}
