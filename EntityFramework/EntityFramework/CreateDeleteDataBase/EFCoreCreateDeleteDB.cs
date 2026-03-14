541a710c8aa272cb0f7497200e9986279470fcc4
MohamedMagdy <mohamedmagdy9452@gmail.com>
2026-03-14 18:55:02 +0200

    feat: Seed Data

---
+++ EntityFramework/EntityFramework/CreateDeleteDataBase/EFCoreCreateDeleteDB.cs
@@ -0,0 +1,34 @@
+﻿using EntityFrameworkCore_DotNet.Data;
+using EntityFrameworkCore_DotNet.Entities;
+
+namespace EntityFrameworkCore_DotNet.CreateDeleteDataBase
+{
+    internal class EFCoreCreateDeleteDB
+    {
+        class Program
+        {
+            public static void Run()
+            {
+                using var context = new AppDbContext();
+
+
+                context.Database.EnsureCreated();
+
+                context.Employees.Add(new Employee
+                {
+                    Name = "Ali"
+                });
+
+                context.Employees.Add(new Employee
+                {
+                    Name = "Sara"
+                });
+
+                context.SaveChanges();
+
+                context.Database.EnsureDeleted();
+            }
+        }
+
+    }
+}
---
+++ EntityFramework/EntityFramework/SeedData/DynamicSeedData.cs
@@ -0,0 +1,29 @@
+﻿using EntityFrameworkCore_DotNet.Data;
+using EntityFrameworkCore_DotNet.Entities;
+
+namespace EntityFrameworkCore_DotNet.SeedData
+{
+    internal class DynamicSeedData
+    {
+        class Program
+        {
+
+            public static void Seed(AppDbContext context)
+            {
+                if (!context.Employees.Any())
+                {
+                    context.Employees.AddRange(
+                        new Employee
+                        {
+                            Name = "Ali"
+                        },
+                        new Employee
+                        {
+                            Name = "Sara"
+                        }
+                    );
+                }
+            }
+        }
+    }
+}
