62f688c51c3bdeb452120df1e6d02136c6931da7
MohamedMagdy <mohamedmagdy9452@gmail.com>
2026-03-15 07:13:05 +0200

    feat : sql query string

---
+++ EntityFramework/EntityFramework/SQLQuery/FromSql.cs
@@ -0,0 +1,21 @@
+﻿using EntityFrameworkCore_DotNet.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntityFrameworkCore_DotNet.SQLQuery
+{
+    internal class FromSql
+    {
+        public static void Run()
+        {
+            using var context = new AppDbContext();
+
+            int empId = 1;
+
+            // استخدام FromSqlRaw بدل FromSql (لأنه FromSql deprecated في EF Core 7+)
+            var c1 = context.Employees
+                .FromSqlRaw($"SELECT * FROM dbo.Employees WHERE Id = {empId}")
+                .FirstOrDefault();
+
+        }
+    }
+}
